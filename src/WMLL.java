1f9c559227b075394d0b249e49eea367db372d86
iPeer <ipeer@ipeerftw.co.cc>
2012-08-28 03:50:47 +0100

    Test 756 -> Stable 31

--- src/WMLL.java
+++ src/WMLL.java
@@ -28,7 +28,7 @@ import reifnsk.minimap.ReiMinimap;
 public class WMLL {
 
 	public static final String wmllVersion() {
-		return "Test 756";
+		return "Stable 31";
 	}
 	public static final List<Integer> blockBlackList = Arrays.asList(0, 8, 7, 9, 44, 20, 130);
 	public static final Map<String, String> fieldNames = new HashMap<String, String>();
